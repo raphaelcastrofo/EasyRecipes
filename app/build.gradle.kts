d4cbc66b5c4aee196ee1d57b0e51fd137234e970
raphaelcastrofo <raphaelcastrofo@gmail.com>
2024-12-12 17:36:06 +0100

    Course challenge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ import java.util.Properties
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    //kotlin("plugin.serialization") version "2.0.0"
 }
 
 android {
